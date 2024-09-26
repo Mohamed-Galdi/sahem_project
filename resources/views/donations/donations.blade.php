8ff22d1bc366064182f6146cb193cbd2ed0a66d5
Mohamed Galdi <contact.galdi@gmail.com>
2024-09-26 13:31:56 +0100

    fix a typo

--- resources/views/donations/donations.blade.php
+++ resources/views/donations/donations.blade.php
@@ -99,7 +99,7 @@
     <div class="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3  gap-12 pt-6 mb-12 px-8 mx-auto max-w-screen-xl ">
         @forelse ($donations as $donation)
             <div class="bg-white  shadow-xl hover:shadow-md rounded-xl dark:bg-gray-800  ">
-                <a href={{ route('donations.show', ['donation' => $donation->id]) }} class=""><img
+                <a href={{ route('donation.show', ['donation' => $donation->id]) }} class=""><img
                         src={{ asset('storage/' . $donation->image_url) }} alt=""
                         class="object-cover w-full h-64 rounded-t-lg "></a>
                 <div class="p-5">
@@ -108,7 +108,7 @@
                             {{ $donation->donation_name }}</h2>
                     </a>
                     <div class="mb-3 font-normal text-gray-700 dark:text-gray-400">{!! $donation->description !!}</div>
-                    <a href={{ route('donations.show', ['donation' => $donation->id]) }} class=" ">
+                    <a href={{ route('donation.show', ['donation' => $donation->id]) }} class=" ">
 
                         <button
                             class="bg-sahem_pr-500 hover:bg-sahem_pr-600 font-semibold py-3 px-5 rounded-full w-full text-light_1 ">قدم
